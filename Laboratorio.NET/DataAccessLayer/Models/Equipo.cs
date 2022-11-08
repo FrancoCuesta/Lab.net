4b802ad79d8cfe18cea1a3cb2816f199bc64f5f7
Franco <81826356+Lolellus@users.noreply.github.com>
2022-11-08 10:58:08 -0300

    equipo

---
+++ Laboratorio.NET/DataAccessLayer/Models/Equipo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Models
+{
+    public class Equipo{
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public int EquipoId { get; set; }
+        public string Nombre { get; set; }
+        public string Escudo { get; set; }
+    }
+}
